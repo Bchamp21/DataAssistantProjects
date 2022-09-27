4987709d55c70ac9a92121ca2e2355b661e2093e
BHUVAN CHANDRA SARAKAM <89426072+Bchamp21@users.noreply.github.com>
2022-09-27 16:20:36 -0500

    Update nlp_test.py

--- nlp_test.py
+++ nlp_test.py
@@ -1,3 +1,16 @@
+import nltk
+
+print(nltk.__version__)
+
+nltk.download()
+
+sentence = """At eight o'clock on Thursday morning
+    Arthur didn't feel very good."""
+
+tokens = nltk.word_tokenize(sentence)
+
+print(tokens)
+
 from nltk.corpus import twitter_samples
 
 positive_tweets = twitter_samples.strings('positive_tweets.json')
